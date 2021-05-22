928723f3378002604916621b7cc66b6cb543f6b2
Pranay Rajput <pranay.rajput@techchefz.com>
2021-05-23 00:13:05 +0530

    My Site

---
+++ core/src/main/java/com/mysite/core/servlets/FetchSearchKeywordsServlet.java
@@ -0,0 +1,56 @@
+package com.mysite.core.servlets;
+
+import com.day.cq.commons.jcr.JcrConstants;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.HttpConstants;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.propertytypes.ServiceDescription;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet that writes some sample content into the response. It is mounted for
+ * all resources of a specific Sling resource type. The
+ * {@link SlingSafeMethodsServlet} shall be used for HTTP methods that are
+ * idempotent. For write operations use the {@link SlingAllMethodsServlet}.
+ */
+@Component(
+        immediate = true,
+        service = {Servlet.class},
+        enabled = true,
+        property = {
+                Constants.SERVICE_DESCRIPTION + "=" + "Fetch Keywords Servlet",
+                "sling.servlet.methods=" + HttpConstants.METHOD_GET,
+                "sling.servlet.paths=" + "/bin/fetchKeyword",
+                Constants.SERVICE_VENDOR + "=" + "Royal Enfield"
+        }
+)
+public class FetchSearchKeywordsServlet extends SlingSafeMethodsServlet {
+
+    //private static final Logger log = LoggerFactory.getLogger(FetchSearchKeywordsServlet.class);
+
+    private static final long serialVersionUID = 1L;
+     //log.info("#### Inside dopost of StoreDataInDBWithAttachmentsInfo ####");
+
+    protected void doGet(final SlingHttpServletRequest req,
+            final SlingHttpServletResponse resp) throws ServletException, IOException {
+//        final Resource resource = req.getResource();
+          req.getParameter("keyWord").toString();
+         resp.setContentType("text/plain");
+//        resp.getWriter().write("Title = " + resource.getValueMap().get(JcrConstants.JCR_TITLE));
+
+      // log.info(req.getParameter("keyWords").toString());
+
+        PrintWriter out = null;
+        out = resp.getWriter();
+        out.println("Requested Keywoard: " + req.getParameter("keyWord").toString());
+    }
+}
